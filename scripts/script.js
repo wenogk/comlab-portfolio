59ba7a45be4025fb8ffb8aab4cd5f86c4fe976f0
wenogk <drf325@nyu.edu>
2020-06-25 20:21:57 +0400

    terminal portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,44 @@
-function getTextWidth(text, font) { //https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript
-    // re-use canvas object for better performance
-    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-    var context = canvas.getContext("2d");
-    context.font = font;
-    var metrics = context.measureText(text);
-    return metrics.width;
-}
+let currentCommandTyped = "";
+document.onkeydown = function(e) { //key press event listener for terminal typing
+    e = e || window.event;
+    console.log(event.keyCode + " pressed!")
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    let letter = String.fromCharCode(charCode).toLowerCase(); //char number to actual letter which is then forced to lowercase so even upper case cheat codes r good
+    if (event.keyCode >= 48 && event.keyCode <= 57) { //if number 0-9
+      currentCommandTyped+= String(event.keyCode-48);
+      $(".new-output").text(currentCommandTyped)
+    } else if (event.keyCode >= 65 && event.keyCode <= 90) { //if letter a-z
+      currentCommandTyped+=letter;
+      $(".new-output").text(currentCommandTyped)
+    } else if(event.keyCode === 32) { // space bar hit
+      console.log("space")
+      currentCommandTyped+=" ";
+      $(".new-output").text(currentCommandTyped)
+    } else if(event.keyCode === 8) { // back space hit
+      currentCommandTyped = currentCommandTyped.slice(0,-1); //remove last character
+      $(".new-output").text(currentCommandTyped)
+    } else if(event.keyCode === 13) { // enter key hit
+      var val = $(this).children($('.404-input')).val().toLowerCase();
+      var href;
+      if(currentCommandTyped=="clear") {
+        $('.terminal').html("");
+      }
+      resetCurrentCommand();
+    }
+};
 
-function letterRepeaterFullWidth(letter) { //function I wrote to repeat a letter for the maximum width of the viewport
-  let maxWidth = $(window).width();
-  let currentWidth = 0;
-  let currentNumberOfLetters = 0;
-  while(maxWidth>currentWidth) {
-    currentNumberOfLetters+=1;
-    currentWidth = getTextWidth(letter.repeat(currentNumberOfLetters), "bold 12px arial")
+function resetCurrentCommand() {
+  let message = "Oops. That command is not recognized."
+  let cleanCommand = currentCommandTyped.replace(/\s+/g,' ').trim();
+  $('.new-output').removeClass('new-output');
+  if(cleanCommand=="") {
+    message="";
+    $('.terminal').append('<p class="prompt output new-output"></p>');
+  } else {
+    $('.terminal').append(`<p class="prompt output">${message}</p><p class="prompt output new-output"></p>`);
   }
-  return letter.repeat((currentNumberOfLetters-1));
-}
-function header(custom = false,textVal = '') {
-  let array = ["romeno","comlab","nyuad","-","©","0","%",":-)","▒"]
-  const text =(custom) ? textVal : array[Math.floor(Math.random() * array.length)];
-  return `<center>${letterRepeaterFullWidth(text)}<br/> | Item | Item 2 | Item 3 | <br/> ${letterRepeaterFullWidth(text)}</center>`;
-}
-function resize(element) { //function I wrote to rescale an element if it exceeds the viewport size
-  let availableWidth = $(window).width();
-  let availableHeight = $(window).height();
-  let contentWidth = $(element).width();
-  let contentHeight = $(element).height();
-  let scale = Math.min(availableWidth / contentWidth, availableHeight / contentHeight ); //https://twitter.com/slides/status/666967622764896257
-  console.log(scale)
-  $(element).css({scale: scale});
-}
 
-$(window).on('resize', function () {
-  $(".header").html(header());
-  resize(".name");
-    });
 
-$(".header").html(header(true,"-resize-me-"));
-
-gsap.to(".plane", {duration: 5, left: 300, bottom: 200, rotation: -270})
+  currentCommandTyped = ""
+  $(".new-output").get(0).scrollIntoView();
+}

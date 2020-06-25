605cdcafba633da91476cecd90309040747b3750
wenogk <drf325@nyu.edu>
2020-06-25 23:07:12 +0400

    open page functionality + tabular projects view

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let currentCommandTyped = "";
+let commandContext = "";
 document.onkeydown = function(e) { //key press event listener for terminal typing
     e = e || window.event;
     console.log(event.keyCode + " pressed!")
@@ -25,6 +26,10 @@ document.onkeydown = function(e) { //key press event listener for terminal typin
     }
 };
 
+function openUrl(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
 function printOnTerminal(txt="") {
   $('.new-output').removeClass('new-output');
   if(txt!="") {
@@ -35,15 +40,42 @@ function printOnTerminal(txt="") {
 }
 
 function runCommand(command) {
+
+  let keepCommandContext = false;
   let c = command.replace(/\s+/g,' ').trim(); //remove any extra white space on either end of string
+  let args = c.split(' ')
   if(c=="clear") {
     $('.terminal').html(`<p class="prompt output new-output"></p>`);
   } else if(c=="help") {
-    printOnTerminal("Commands available are: clear");
-  } else if(c=="") {
+    printOnTerminal("▬▬ι═══════ﺤ Commands available are: projects, about, message, clear  -═══════ι▬▬");
+  } else if(c=="projects") {
+    var table = new AsciiTable('Projects')
+table
+  .setHeading('Name','Command for more info')
+  .addRow('Convinience Store Tour', "a1")
+  .addRow('Period.s comic', "a2")
+  .addRow('Flight: A turbulent experience', "a3")
+  .addRow('GTA NYU Abu Dhabi', "a4")
+
+printOnTerminal("<br />"+ table.toString())
+
+} else if(c.startsWith("open ") && args.length>1) {
+  switch(args[1]) {
+    case "a1":
+      openUrl("https://wenogk.github.io/comlab-assignment-1/")
+    default:
+      printOnTerminal("Incorrect assignment name.")
+  }
+  printOnTerminal("Open the assignment " + args[1])
+} else if(c=="") {
     printOnTerminal();
   } else {
     printOnTerminal(`Oops. That command is not recognized. Type in "help"`)
+  }
+  if(keepCommandContext) {
+    commandContext = command;
+  } else {
+    commandContext = "";
   }
   currentCommandTyped = ""
   $(".new-output").get(0).scrollIntoView();
